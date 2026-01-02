0f0446f7ad9507b5a3e55c84959aa42915e29d80
ramadienn~ <muhammad.ramadienn@gmail.com>
2026-01-02 15:37:06 +0700

    chore: remove all files

--- get_hash_password.php
+++
@@ -1,5 +0,0 @@
-<?php
-echo password_hash('admin123', PASSWORD_DEFAULT); // Ini akan menghasilkan hash untuk 'admin123'
-echo "<br>";
-echo password_hash('petugas123', PASSWORD_DEFAULT); // Ini akan menghasilkan hash untuk 'petugas123'
-?>
